70e4864c203688574acbd4ba7c4e4e2678f30436
mannasoumya <sm323265@gmail.com>
2020-08-19 21:15:23 +0530

    Implemented Unchanged Algorithm written in the paper in the first link

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let random = Math.random();
 let regretSum = new Array(NUM_ACTIONS),
   strategy = new Array(NUM_ACTIONS),
   strategySum = new Array(NUM_ACTIONS),
-  oppStrategy = [0.4, 0.3, 0.3];
+  oppStrategy = [0.4, 0.55, 0.05];
 
 function getStrategy() {
   let normalizingSum = 0;
@@ -36,14 +36,41 @@ function getAction(strategy) {
   return a;
 }
 
+//TRAIN the strategy model
 function train(iters) {
   let actionUtility = new Array(NUM_ACTIONS);
   for (let i = 0; i < iters; i++) {
     //Get regret-matched mixed-strategy actions
+    let strategy = getStrategy();
+    let myaction = getAction(strategy);
+    let otheraction = getAction(oppStrategy);
     //Compute action utilities
+    actionUtility[otheraction] = 0;
+    actionUtility[otheraction == NUM_ACTIONS - 1 ? 0 : otheraction + 1] = 1;
+    actionUtility[otheraction == 0 ? NUM_ACTIONS - 1 : otheraction - 1] = -1;
     //Accumulate action regrets
+    for (let a = 0; a < NUM_ACTIONS; a++)
+      regretSum[a] += actionUtility[a] - actionUtility[myaction];
   }
 }
+
+//Get average mixed strategy across all training iterations
+function getAverageStrategy() {
+  let avgStrategy = new Array(NUM_ACTIONS);
+  let normalizingSum = 0;
+  for (let a = 0; a < NUM_ACTIONS; a++) normalizingSum += strategySum[a];
+  for (let a = 0; a < NUM_ACTIONS; a++) {
+    if (normalizingSum > 0) {
+      avgStrategy[a] = strategySum[a] / normalizingSum;
+    } else {
+      avgStrategy[a] = 1 / NUM_ACTIONS;
+    }
+  }
+  return avgStrategy;
+}
+
+train(1000000);
+console.log(getAverageStrategy());
 
 // let strategy_i = getStrategy();
 // console.log(getAction(strategy_i));
